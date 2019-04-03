c88a5789d64c0abb0c2027dd362be943da602e8a
TeeNik <teenik3315@gmail.com>
2019-04-03 21:09:00 +0300

    weight quick union

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import Algorithm.WeightQuickUnion;
+import Model.Union;
+import Reader.UnionReader;
+
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello World!");
+
+        UnionReader reader = new UnionReader();
+        ArrayList<Union> list = reader.readUnion("data.txt");
+
+        WeightQuickUnion wqu = new WeightQuickUnion(10);
+        for (Union u : list) {
+            wqu.union(u);
+        }
+        wqu.print();
     }
 }
---
+++ src/Model/Union.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Union {
+
+    public final int x;
+    public final int y;
+
+    public Union(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
