feb853eabe6798e3aff559baa5181d840ffffd35
TeeNik <teenik3315@gmail.com>
2019-04-06 23:50:14 +0300

    perlocation [2]

--- src/Algorithm/Percolation.java
+++ src/Algorithm/Percolation.java
@@ -1,6 +1,10 @@
 package Algorithm;
 
+import Model.Union;
+import Reader.UnionReader;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+import java.util.ArrayList;
 
 public class Percolation {
 
@@ -21,18 +25,34 @@ public class Percolation {
     }
 
     public void open(int row, int col)  {
-        opened[row-1][col] = true;
+        opened[row-1][col-1] = true;
         numOfOpen++;
 
-        int index = getSiteIndex(row, col)
+        int index = getSiteIndex(row, col);
 
         if(row == 1){
-            wq.union(getSiteIndex(r));
+            wq.union(index, top);
+        }
+        if(row == size){
+            wq.union(index, bottom);
+        }
+
+        if(row > 1 && isOpen(row-1, col)){
+            wq.union(index, getSiteIndex(row-1, col));
+        }
+        if(row < size && isOpen(row+1, col)){
+            wq.union(index, getSiteIndex(row+1, col));
+        }
+        if(col > 1 && isOpen(row, col-1)){
+            wq.union(index, getSiteIndex(row, col-1));
+        }
+        if(col < size && isOpen(row, col+1)){
+            wq.union(index, getSiteIndex(row, col+1));
         }
     }
 
     public boolean isOpen(int row, int col) {
-        return opened[row][col];
+        return opened[row-1][col-1];
     }
 
     public boolean isFull(int row, int col) {
@@ -44,7 +64,7 @@ public class Percolation {
     }
 
     public boolean percolates() {
-
+        return wq.connected(top, bottom);
     }
 
     private int getSiteIndex(int row, int col){
@@ -52,7 +72,14 @@ public class Percolation {
     }
 
     public static void main(String[] args)  {
+        UnionReader reader = new UnionReader();
+        ArrayList<Union> list = reader.readUnion("data.txt");
 
+        Percolation percolation = new Percolation(3);
+        for (Union union : list) {
+            percolation.open(union.x, union.y);
+        }
+        System.out.println("percolates: " + percolation.percolates());
     }
 
 }
