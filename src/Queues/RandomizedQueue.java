45bc362d12f8af911d5a553cca7cd377d9315f6d
TeeNik <teenik3315@gmail.com>
2019-04-14 11:43:58 +0300

    random queue

--- src/Queues/RandomizedQueue.java
+++ src/Queues/RandomizedQueue.java
@@ -1,81 +1,115 @@
 package Queues;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
     public class QueueIterator implements Iterator<Item>{
 
-        private Node current = first;
+        private int length = array.length;
+        private Item[] copyArray = (Item[]) new Object[length];
+
+        public QueueIterator(){
+            for (int i = 0; i < length; ++i){
+                copyArray[i] = array[i];
+            }
+        }
 
         @Override
         public boolean hasNext() {
-            return current != null;
+            return length != 0;
         }
 
         @Override
         public Item next() {
-            Item item = current.value;
-            current = current.next;
+            if(length == 0){
+                throw new NoSuchElementException();
+            }
+            int rand = StdRandom.uniform(length);
+            Item item = copyArray[rand];
+            if(rand != length-1){
+                array[rand] = array[length-1];
+            }
+            array[length-1] = null;
+            --length;
             return item;
         }
     }
 
-    private int size;
-    private Node first;
-    private Node last;
-
-    private class Node{
-        Item value;
-        Node next;
-        Node prev;
-
-        Node(Item item){
-            if(item == null){
-                throw new IllegalArgumentException();
-            }
-            value = item;
-        }
-    }
+    private int current;
+    private Item[] array;
 
     public RandomizedQueue() {
-
+        array = (Item[])new Object[2];
     }
 
     public boolean isEmpty() {
-        return size == 0;
+        return current == 0;
     }
 
     public int size() {
-        return size;
+        return current;
+    }
+
+    private void resize(int newSize){
+        Item[] copyArray = (Item[]) new Object[newSize];
+        for (int i = 0; i < current; ++i){
+            copyArray[i] = array[i];
+        }
+        array = copyArray;
     }
 
     public void enqueue(Item item) {
-        Node newFirst = new Node(item);
-        newFirst.next = first;
-        first.prev = newFirst;
-        last = first;
-        first = newFirst;
+        if(current == array.length){
+            resize(current*2);
+        }
+        array[current] = item;
+        ++current;
     }
 
     public Item dequeue() {
-        Item item = last.value;
-        Node newLast = last.prev;
-        newLast.next = null;
-        last = newLast;
-        
+        int rand = StdRandom.uniform(current);
+        Item item = array[rand];
+        if(rand != current-1){
+            array[rand] = array[current-1];
+        }
+        array[current-1] = null;
+        if(current == array.length / 4){
+            resize(array.length/2);
+        }
+        --current;
         return item;
     }
 
     public Item sample() {
-
+        int rand = StdRandom.uniform(current);
+        Item item = array[rand];
+        if(current == array.length / 4){
+            resize(array.length/2);
+        }
+        return item;
     }
 
     public Iterator<Item> iterator() {
-
+        return new QueueIterator();
     }
 
     public static void main(String[] args) {
+        RandomizedQueue<Integer> queue = new RandomizedQueue<Integer>();
+        queue.enqueue(2);
+        queue.enqueue(1);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(7);
+        queue.enqueue(5);
+        queue.enqueue(6);
 
+        int length = queue.size();
+        for(int i = 0; i < length; ++i){
+            System.out.println(queue.dequeue());
+        }
     }
 }
