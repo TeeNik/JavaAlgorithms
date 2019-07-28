fd59eae405322be3e41ecbdeb1ec34e7bb1b17a5
TeeNik <teenik3315@gmail.com>
2019-07-29 00:09:00 +0300

    seam carver [2]

--- src/SeamCarving/SeamCarver.java
+++ src/SeamCarving/SeamCarver.java
@@ -2,9 +2,30 @@ import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
 
+    private final double BORDER_ENERGY = 195075.0;
+
+    private int[][] colors;
+    private double[][] distTo;
+    private short[][] edgeTo;
+    private int width;
+    private int height;
+    private final Picture picture;
+
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture){
+        this.picture = picture;
+        width = picture.width();
+        height = picture.height();
 
+        colors = new int[width][height];
+        edgeTo = new short[width][height];
+        distTo = new double[width][height];
+
+        for (int i = 0; i < width; ++i){
+            for (int j = 0; j < height; ++j){
+                colors[i][j] = picture.getRGB(i, j);
+            }
+        }
     }
 
     // current picture
@@ -14,12 +35,12 @@ public class SeamCarver {
 
     // width of current picture
     public int width(){
-
+        return width;
     }
 
     // height of current picture
     public int height(){
-
+        return height;
     }
 
     // energy of pixel at column x and row y
