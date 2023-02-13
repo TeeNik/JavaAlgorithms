1f0083aa0bc9cb076243ee0be0b1e6177c73fb98
TeeNik <teenik3315@gmail.com>
2023-02-14 02:38:16 +0600

    Permutation final

--- src/Permutation/Deque.java
+++ src/Permutation/Deque.java
@@ -1,3 +1,5 @@
+package Permutation;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
--- src/Permutation/Permutation.java
+++ src/Permutation/Permutation.java
@@ -1,3 +1,5 @@
+package Permutation;
+
 import edu.princeton.cs.algs4.StdIn;
 
 public class Permutation {
