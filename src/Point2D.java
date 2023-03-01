6f21680b432523b763a676ab57de4408b2137724
TeeNik <teenik3315@gmail.com>
2023-03-01 15:54:06 +0600

    Point2D

--- src/Point2D.java
+++ src/Point2D.java
@@ -1,47 +1,57 @@
+import edu.princeton.cs.algs4.StdDraw;
+
 public class Point2D implements Comparable<Point2D> {
+
+    private double x;
+    private double y;
 
     // construct the point (x, y)
     public Point2D(double x, double y) {
-
+        this.x = x;
+        this.y = y;
     }
 
     // x-coordinate
     public double x() {
-        return 0;
+        return x;
     }
 
     // y-coordinate
     public double y() {
-        return 0;
+        return y;
     }
 
     // Euclidean distance between two points
     public double distanceTo(Point2D that) {
-        return -1;
+        return Math.sqrt((that.x - x) * (that.x - x) + (that.y - y) * (that.y));
     }
 
     // square of Euclidean distance between two points
     public double distanceSquaredTo(Point2D that) {
-        return -1;
+        return (that.x - x) * (that.x - x) + (that.y - y) * (that.y);
     }
 
     // for use in an ordered symbol table
     public int compareTo(Point2D that) {
-        return 0;
+        return Double.compare(x, that.x);
     }
 
     // does this point equal that object?
-    public boolean equals(Object that) {
-        return false;
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Point2D) || o == null) return false;
+
+        Point2D that = (Point2D) o;
+        return that.x == x && that.y == y;
     }
 
     // draw to standard draw
     public void draw() {
-
+        StdDraw.circle(x, y, 5);
     }
 
     // string representation
     public String toString() {
-        return null;
+        return "(" + x + "," + y + ")";
     }
 }
